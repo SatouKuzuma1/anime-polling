51f63ac98d21a0b1da64f24f906a44bec6fa36a6
ruben <ruben71999@icloud.com>
2024-04-08 13:59:04 +0200

    Ignore Build Errors in next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     // domains: {},
     remotePatterns: [
