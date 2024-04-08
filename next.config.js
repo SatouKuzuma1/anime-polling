ca7c465537eb949b8ce298b6a7dc47f12c19a592
ruben <ruben71999@icloud.com>
2024-04-08 14:06:03 +0200

    Ignore Build Errors in next config

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const config = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     // domains: {},
     remotePatterns: [
